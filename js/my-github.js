401220d27fc943b21ebbd284e536cda5693f19fc
nabraha <nebaytedros@yahoo.com>
2019-04-23 19:08:26 +0100

    My GitHub Project Part 7

--- js/my-github.js
+++ js/my-github.js
@@ -1,14 +1,73 @@
 // Write code here to communicate with Github
 
-var url = "https://api.github.com/users/nabraha/repos";
-fetch(url)
-  .then(resp => resp.json())
-  .then(function(result) {
-    numberOfRepos(result);
-    result.forEach(function(name) {
-      reposListAndLink(name);
+//Part-1
+// var url = "https://api.github.com/users/nabraha/repos";
+// fetch(url)
+//   .then(resp => resp.json())
+//   .then(function(result) {
+//     numberOfRepos(result);
+//     result.forEach(function(name) {
+//       reposListAndLink(name);
+//     });
+//   });
+
+// function reposListAndLink(name) {
+//   const getRepoId = document.getElementById("repos-list");
+//   const repoItemElement = document.createElement("li");
+//   repoItemElement.appendChild(addAnchor(name));
+//   getRepoId.appendChild(repoItemElement);
+// }
+
+// function addAnchor(name) {
+//   var addLink = document.createElement("a");
+//   addLink.href = name.html_url;
+//   addLink.text = name.name;
+//   return addLink;
+// }
+
+// function numberOfRepos(result) {
+//   var repoNumber = document.getElementById("repos-count");
+//   repoNumber.innerText = result.length;
+//   //  repoNumber.innerText = Object.keys(result).length;
+// }
+
+//part-3
+
+var repoNumber = document.getElementById("repos-count");
+const getRepoId = document.getElementById("repos-list");
+const getForm = document.querySelector(".my-auto form:nth-child(4)");
+const defaultUser = "Nabraha";
+getForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  var value = getForm.querySelector('input[type="text"]').value.toLowerCase();
+  if (value === "") {
+    value = defaultUser;
+    console.log("val 1", value);
+  }
+
+  var url = "https://api.github.com/users/" + value + "/repos";
+  fetch(url)
+    .then(resp => resp.json())
+    .then(function(result) {
+      result.forEach(function(name) {
+        if (defaultUser === name.owner.login) {
+          repoNumber.innerText = result.length;
+          reposListAndLink(name);
+        } else if (value === name.owner.login) {
+          const getDiv = document.querySelector(".my-auto div:nth-child(2)");
+          repoNumber.innerText = result.length;
+          getDiv.innerHTML =
+            name.owner.login +
+            " has " +
+            repoNumber.innerText +
+            " repos on GITHUB.";
+          reposListAndLink(name);
+        }
+      });
     });
-  });
+  getRepoId.innerText = "";
+});
 
 function reposListAndLink(name) {
   const getRepoId = document.getElementById("repos-list");
@@ -20,12 +79,6 @@ function reposListAndLink(name) {
 function addAnchor(name) {
   var addLink = document.createElement("a");
   addLink.href = name.html_url;
-  addLink.text = name.name;
+  addLink.text = name.owner.login;
   return addLink;
-}
-
-function numberOfRepos(result) {
-  var repoNumber = document.getElementById("repos-count");
-  repoNumber.innerText = result.length;
-  //  repoNumber.innerText = Object.keys(result).length;
 }
